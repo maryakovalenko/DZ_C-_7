bb027d230d383adfcb95e43266bec26bd563d034
maryakovalenko <pomashka18061992@gmail.com>
2022-07-28 17:36:34 +0300

    DZ_7_1, DZ_7_2, DZ_7_3

---
+++ DZ_7_2/Program.cs
@@ -0,0 +1,63 @@
+﻿// Знакомство с языками программирования,домашняя работа к уроку 7. Как не нужно писать код.
+Console.WriteLine("Задача № 50");
+Console.WriteLine("----------------------");
+
+Console.Write("\nMассив возьмем из предыдущей задачи (№ 47).\n");
+Console.Write("Введите координаты позиции элемента, разделенных запятой: ");
+
+string? positionElement = Console.ReadLine();
+positionElement = RemovingSpaces(positionElement);
+int[] position = ParserString(positionElement);
+
+if(position[0] <= 3
+&& position[1] <= 4 
+&& position[0] >= 0 
+&& position[1] >= 0) 
+{
+ 
+  Console.Write($"Значение элемента:  ");
+}
+else Console.Write($"такого элемента в массиве нет.");
+
+int[] ParserString(string input)
+{
+  int countNumbers = 1;
+  for (int i = 0; i < input.Length; i++)
+  {
+      if (input[i] == ',')
+          countNumbers++;
+  }
+
+  int[] numbers = new int[countNumbers];
+
+  int numberIndex = 0;
+  for(int i = 0; i < input.Length; i++)
+  {
+    string subString = String.Empty;
+
+    while (input[i] != ',')
+    {
+      subString += input[i].ToString();
+      if (i >= input.Length - 1)
+        break;
+      i++;
+    }
+    numbers[numberIndex] = Convert.ToInt32(subString);
+    numberIndex++;
+  }
+
+  return numbers;
+}
+
+string RemovingSpaces (string input)
+{
+  string output = String.Empty;
+  for (int i = 0; i < input.Length; i++)
+  {
+    if (input[i] != ',') 
+    {
+      output += input[i];
+    }
+  }
+  return output;
+}
---
+++ DZ_7_3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Знакомство с языками программирования,домашняя работа к уроку 7. Как не нужно писать код.
+Console.WriteLine("Задача № 52");
+Console.WriteLine("------------------------------");
+
+int[,] arrayWhole = new int[3,4];
+
+Console.Write($"\nCреднее арифметическое:\n");
+for (int i = 0; i < 3; i++)
+{
+  double arithmeticMean = 0;
+  for (int j = 0; j < 4; j++)
+  {
+    arithmeticMean += arrayWhole[j, i];
+  }
+  arithmeticMean = Math.Round(arithmeticMean / 3, 1);
+  Console.WriteLine($"столбца № {i+1} {arithmeticMean}");
+}
+
+int[,] TransformationArrayWhole (double[,] array)
+{
+  int[,] arrayWhole = new int[array.GetLength(0), array.GetLength(1)];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      arrayWhole[i, j] = Convert.ToInt32(array[i, j]);
+    }
+  }
+  return arrayWhole;
+}
+
+void WriteArrayInt (int[,] arrayWhole){
+for (int i = 0; i < 3; i++)
+  {
+      for (int j = 0; j < 4; j++)
+      {
+        Console.Write(arrayWhole[i, j] + " ");
+      }
+      Console.WriteLine();
+  }
+}
